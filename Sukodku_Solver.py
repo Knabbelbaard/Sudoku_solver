808e03cd10bdaa5031fdafee00bf2407f7a7f091
Knabbelbaard <joukedj@xs4all.nl>
2025-05-05 13:04:02 +0200

    input

--- Sukodku_Solver.py
+++ Sukodku_Solver.py
@@ -1,39 +1,55 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
     
 
-@app.route('/')
+# sudoku_board = [
+    #     [0, 0, 0, 0, 7, 0, 5, 1, 0],
+    #     [0, 0, 7, 0, 0, 0, 0, 8, 2],
+    #     [0, 0, 0, 9, 2, 8, 0, 0, 0],
+    #     [0, 3, 0, 0, 0, 4, 0, 0, 0],
+    #     [7, 9, 0, 0, 0, 0, 0, 6, 1],
+    #     [0, 0, 0, 7, 0, 0, 0, 3, 0],
+    #     [0, 0, 0, 4, 6, 9, 0, 0, 0],
+    #     [2, 1, 0, 0, 0, 0, 8, 0, 0],
+    #     [0, 5, 6, 0, 1, 0, 0, 0, 0]
+    # ]
+
+
+def create_sudoku_board(values):
+    return [values[i:i+9] for i in range(0, 81, 9)]
+
+
+@app.route('/', methods=["GET", "POST"])
 def index():
-    sudoku_board = [
-        [0, 0, 0, 0, 7, 0, 5, 1, 0],
-        [0, 0, 7, 0, 0, 0, 0, 8, 2],
-        [0, 0, 0, 9, 2, 8, 0, 0, 0],
-        [0, 3, 0, 0, 0, 4, 0, 0, 0],
-        [7, 9, 0, 0, 0, 0, 0, 6, 1],
-        [0, 0, 0, 7, 0, 0, 0, 3, 0],
-        [0, 0, 0, 4, 6, 9, 0, 0, 0],
-        [2, 1, 0, 0, 0, 0, 8, 0, 0],
-        [0, 5, 6, 0, 1, 0, 0, 0, 0]
-    ]
-    if solve(sudoku_board):
-        return render_template('index.html', board=print_sudoku_board(sudoku_board)) #Response(print_sudoku_board(sudoku_board), mimetype='text/plain')
+    if request.method == "POST":
+        values = request.form.getlist('cel')
+        sudoku_board = create_sudoku_board([int(v) if v.isdigit() else 0 for v in values])
     else:
-        return "No solution exists"
+        sudoku_board = [[0]*9 for _ in range(9)]
+    board_str = create_sudoku_board(sudoku_board)
+    return render_template("index.html", board=board_str)
+    
+    # if solve(sudoku_board):
+    #     board_str = print_sudoku_board(sudoku_board)
+    #     return render_template("index.html", board=board_str)
+    # else:
+    #     return "No solution exists"
     
 
+
 # Function to print the Sudoku board
-def print_sudoku_board(board):
-    board_str = ""
-    for i, row in enumerate(board):
-        if i % 3 == 0 and i != 0:
-            board_str += "-" * 21 + "\n"
-        for j, value in enumerate(row):
-            if j % 3 == 0 and j != 0:
-                board_str += "| "
-            board_str += str(value if value != 0 else ".") + " "
-        board_str += "\n"
-    return board_str
+#  def print_sudoku_board(board):
+#     board_str = ""
+#     for i, row in enumerate(board):
+#         if i % 3 == 0 and i != 0:
+#             board_str += "-" * 21 + "\n"
+#         for j, value in enumerate(row):
+#             if j % 3 == 0 and j != 0:
+#                 board_str += "| "
+#             board_str += str(value if value != 0 else ".") + " "
+#         board_str += "\n"
+#     return board_str
 
 
 # Function to find an empty location in the Sudoku board
